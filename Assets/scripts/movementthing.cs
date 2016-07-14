ca9ef75d1d51f4905af07bd3ba0a9e43e447d665
Cyle <cmillarlay@gmail.com>
2016-07-13 22:30:21 -0700

    new

---
+++ Assets/scripts/movementthing.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movementthing : MonoBehaviour {
+
+    Rigidbody _cachedRB;
+    private Vector3 _move;
+
+    public void Start()
+    {
+        _cachedRB = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 move)
+    {
+        _move += move;
+    }
+
+    public void FixedUpdate()
+    {
+        _cachedRB.AddForce(_move);
+        _move = Vector3.zero;
+    }
+}
---
+++ Assets/scripts/playermove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playermove : MonoBehaviour {
+
+    
+    public movementthing move;
+    [Range(50,700)]
+    public float speed;
+    [Range(200,700)]
+    public float jumpspeed = 100.0f;
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            transform.Translate(Vector3.up * jumpspeed * Time.deltaTime, Space.World);
+        }
+
+        float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float v = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        move.Move(new Vector3(h, 0f, v));
+    }
+}
